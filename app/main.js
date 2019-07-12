c286cb88d14e0bd0981f03e4bd41a03d64f1e167
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-07-12 15:52:20 -0700

    listing clippings

--- app/main.js
+++ app/main.js
@@ -1,7 +1,14 @@
 const  { app, Tray, Menu, systemPreferences } = require('electron');
 const path = require('path');
 
+const clippings  = [];
 let tray = null;
+
+const getIcon = () => {
+  if (process.platform === 'win32') return 'icon-light2x.ico';
+  if (systemPreferences.isDarkMode()) return 'icon-light.png';
+  return 'icon-dark.png';
+};
 
 app.on('ready' , () => {
   if (app.dock) app.dock.hide();
@@ -11,17 +18,17 @@ app.on('ready' , () => {
     tray.on('click', tray.popUpContextMenu);
   }
 
-  const menu = Menu.buildFromTemplate([
-    { label: 'Quit', click() { app.quit(); } }
-  ]);
-
+  updateMenu();
   tray.setToolTip('Clipmaster');
-  tray.setContextMenu(menu);
-
 });
 
-const getIcon = () => {
-  if (process.platform === 'win32') return 'icon-light2x.ico';
-  if (systemPreferences.isDarkMode()) return 'icon-light.png';
-  return 'icon-dark.png';
+const updateMenu = () => {
+  const menu = Menu.buildFromTemplate([
+    { label: 'Create New Clipping', click() { null; } },
+    { type: 'separator' },
+    ...clippings.map((clipping, index) => ({ label: clipping })),
+    { label: 'Quit', click() { app.quit(); } }
+  ]);
+  tray.setContextMenu(menu);
+
 };
