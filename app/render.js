1506e81d8584ba45867c344f5b84aa49a011a4c7
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-07-13 19:25:39 -0700

    added clipboard functionality to renderer.js

--- app/render.js
+++ app/render.js
@@ -1,5 +1,29 @@
-const { ipcRenderer } = require('electron');
+const { clipboard } = require('electron');
 
-ipcRenderer.on('show-notification', (evt, title, body) => {
-  const myNotification = new Notification(title, { body });
-});
+const clippingsList = document.getElementById('clippings-list');
+const copyFromClipboardButton = document.getElementById('copy-from-clipboard');
+
+copyFromClipboardButton.addEventListener('click', addClippingToList);
+
+const addClippingToList = () => {
+  const clippingText = clipboard.readText;
+  const clippingElement = createClippingElement(clippingText);
+  clippingsList.prepend(clippingElement);
+};
+
+const createClippingElement = (clippingText) => {
+  const clippingElement = document.createElement('article');
+
+  clippingElement.classList.add('clippings-list-item');
+  clippingElement.innerHTM = `
+    <div class='clipping-text' disabled='true'></div>
+    <div class='clipping-control'>
+      <button class='copy-clipping'>&arr; Clipboard</button>
+      <button class='publish-clipping'>Publish</button>
+      <button class=''remove-clipping>Remove</div>
+    </div>
+  `;
+  clippingElement.querySelector('.clipping-text').innerText = clippingText;
+
+  return clippingElement;
+};
